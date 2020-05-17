c98b2c3b1d158a758100661544714c5d68972322
Ante Pettersson <ante.pettersson@gmail.com>
2020-05-16 23:01:39 -0400

    changed name of file containing scripts to match assignment

---
+++ pollutantmean.R
@@ -0,0 +1,24 @@
+pad_zeros <- function (input, target_len=3){
+   input_str <- as.character(input)
+   n_zeros <- target_len - nchar(input_str)
+   if (n_zeros<=0){
+      input_str
+   }
+   else {
+      for (i in 1:n_zeros) {
+         input_str <- paste ('0', input_str, sep='')
+      }
+      input_str
+   }
+}
+
+pollutantmean <- function (directory, pollutant, id=1:332){
+   data <- NULL
+   for (number in id){
+      filename <- paste (directory, '/', pad_zeros(number), ".csv", sep='')
+      file <- read.csv(filename)
+      datacol <- file[[pollutant]]
+      data <- c(data, datacol[!is.na(datacol)])
+   }
+   mean(data)
+}
